8e7a6ed7be0a20746cf232484eae51d2a77e5415
BjornFJohansson <bjorn_johansson@bio.uminho.pt>
2017-03-12 00:49:17 +0000

    1st commit

--- ci.sh
+++ ci.sh
@@ -1,12 +1,16 @@
-#!/usr/bin/env bash 
+#!/usr/bin/env bash
 wget -q https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O Miniconda_latest.sh
 bash Miniconda_latest.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
 rm Miniconda_latest.sh
-conda update -yq conda
+conda config --set binstar_upload no --set always_yes yes --set show_channel_urls yes
+conda update -q conda
 conda config --add channels BjornFJohansson
-conda create -q -y -n testenv python=3.5 pydna nbval pytest lxml requests
+conda create -q -n testenv python=3.5 pydna nbval pytest lxml requests
 source activate testenv
 which python
 python --version
+conda install pydna
+
+
 python run_test.py
