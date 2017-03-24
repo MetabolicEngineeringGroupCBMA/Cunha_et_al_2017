52ddc27ba360c6b07b25f25e4de411611493abc3
BjornFJohansson <bjorn_johansson@bio.uminho.pt>
2017-03-24 20:36:01 +0000

    conda list

--- ci.sh
+++ ci.sh
@@ -6,6 +6,7 @@ rm Miniconda_latest.sh
 
 conda config --set binstar_upload no --set always_yes yes --set show_channel_urls yes
 #conda update conda
+conda install anaconda-client
 conda env create -f environment.yml
 source activate mybinder_pygenome
 conda install nbval pytest
