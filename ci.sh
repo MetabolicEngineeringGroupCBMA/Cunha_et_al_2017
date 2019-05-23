5cab88d0b6d871df1521c7441c18aae90f94ca64
BjornFJohansson <bjorn_johansson@bio.uminho.pt>
2019-05-23 12:03:42 +0100

    pydna3 works

--- ci.sh
+++ ci.sh
@@ -1,35 +1,26 @@
 #!/usr/bin/env bash
-if [[ $CI = true ]]||[[ $CI = True ]]
-then
-    echo "Running on CI server"
-    wget -q https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O Miniconda_latest.sh
-    bash Miniconda_latest.sh -b -p $HOME/miniconda  
-    export PATH="$HOME/miniconda/bin:$PATH"
-    rm Miniconda_latest.sh
-    conda update --yes -q conda
-    conda config --set always_yes yes --set show_channel_urls yes
-    conda config --add channels BjornFJohansson
-    conda config --add channels conda-forge
-else
-    echo "Not running on CI server"
-fi
+wget -q https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O Miniconda_latest.sh
+bash Miniconda_latest.sh -b -p $HOME/miniconda
+export PATH="$HOME/miniconda/bin:$PATH"
+rm Miniconda_latest.sh
+conda config --set always_yes yes --set show_channel_urls yes
+conda update conda
+conda config --add channels conda-forge
+conda create -qy -n testenv python=3.7 nbconvert pytest nbval termcolor
+source activate testenv
+which python
+python --version
 
-#conda update conda
-conda --version
-conda env create -f environment.yml
+shopt -s globstar
 
-source activate mybinder_pygenome
-conda install nbval pytest
-
-python run_test.py
+#conda install <optional dependencies>
 
 
-if [[ $CI = true ]]||[[ $CI = True ]]
-then
-    echo "Running on CI server"
-else
-    echo "Not running on CI server"
-    source activate bjorn3
-    conda remove --all -n mybinder_pygenome
-fi
+# run all notebooks in current folder and subfolders
+jupyter nbconvert --ExecutePreprocessor.kernel_name=python3 --execute --inplace --allow-errors notebooks/**/[^_^.]*.ipynb
 
+# pytest all notebooks in current folder and subfolders
+pytest --current-env --verbose --capture=no --nbval notebooks/**/[^_^.]*.ipynb
+
+# convert all notebooks in current folder and subfolders to html
+jupyter nbconvert notebooks/**/[^_^.]*.ipynb
