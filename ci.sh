823079922ea88c974f51df904b272b63e2c9d871
BjornFJohansson <bjorn_johansson@bio.uminho.pt>
2019-06-09 17:43:26 +0100

    fix ci

--- ci.sh
+++ ci.sh
@@ -24,13 +24,13 @@ shopt -s globstar
 cd notebooks
 
 # command to run all notebooks
-jupyter nbconvert --ExecutePreprocessor.kernel_name=python3 --execute --inplace --allow-errors **/*.ipynb
+# jupyter nbconvert --ExecutePreprocessor.kernel_name=python3 --execute --inplace --allow-errors **/*.ipynb
 
 #command to convert all notebooks
-jupyter nbconvert **/*.ipynb
+# jupyter nbconvert **/*.ipynb
 
 #command to test all notebooks
-pytest --current-env --verbose --capture=no --nbval **/*.ipynb
+pytest --current-env --capture=no --nbval **/*.ipynb
 
 
 
