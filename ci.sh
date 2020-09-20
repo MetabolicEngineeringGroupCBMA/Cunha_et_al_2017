24d46fbe440da50d89d4391eee4dc4b4ce5a66d6
BjornFJohansson <bjorn_johansson@bio.uminho.pt>
2020-09-20 14:55:03 +0100

    notebooks passes

--- ci.sh
+++ ci.sh
@@ -1,18 +1,3 @@
-#!/usr/bin/env bash
-
-wget -q https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O Miniconda_latest.sh
-bash Miniconda_latest.sh -b -p $HOME/miniconda
-export PATH="$HOME/miniconda/bin:$PATH"
-rm Miniconda_latest.sh
-
-
-conda config --set always_yes yes --set show_channel_urls yes
-conda update conda
-conda env create -f environment.yml
-source activate testenvironment
-conda config --add channels conda-forge
-conda install pytest nbconvert nbval termcolor
-
 
 # This is to make sure double stars ** works
 shopt -s globstar
